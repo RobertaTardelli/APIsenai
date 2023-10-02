36fa6574fe2c82ec1f65cea279f1ac8e11fefb5d
Roberta Moura Tardelli <PET0142070@aluno.firjansenaisesi.com.br>
2023-10-02 09:20:54 -0300

    api inicio

---
+++ src/main/java/com_api_senai/api/ApiApplication.java
@@ -0,0 +1,19 @@
+package com_api_senai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+
+		System.out.println("Hello World!!");
+	}
+
+
+
+}
---
+++ src/test/java/com_api_senai/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com_api_senai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
